e97c4667f34528b0c982f299c836d91f9006859a
Oded <odedatias8115@gmail.com>
2022-11-06 12:37:54 +0200

    Adding basic validation

--- src/Components/Users/UserSearch.jsx
+++ src/Components/Users/UserSearch.jsx
@@ -1,9 +1,18 @@
 import { FiSearch } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import GithubContext from '../../Context/Github/GithubContext';
+import Joi from 'joi-browser';
 
 const UserSearch = () => {
 	const [text, setText] = useState('');
+	const { users } = useContext(GithubContext);
 
+	//Joi Validation Schema
+	const schema = Joi.object({
+		text: Joi.string().trim().required().label('Username'),
+	});
+
+	//UserSearch handlers
 	const handleChange = ({ target: input }) => {
 		setText(input.value);
 	};
@@ -11,20 +20,26 @@ const UserSearch = () => {
 	const handleSubmit = e => {
 		e.preventDefault();
 		//Validation
+		const { error } = Joi.validate({ text }, schema);
+		if (error) alert(error.details[0].message);
+		else {
+			//Search users
+			console.log('search');
+		}
 	};
 
 	return (
-		<div className='grid grid-cols-2 mb-8 gap-8'>
-			<div>
+		<div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+			<div className='col-span-1'>
 				<form onSubmit={handleSubmit}>
 					<div className='form-control'>
 						<div className='relative'>
 							<input
+								onChange={handleChange}
 								type='text'
 								className='w-full bg-gray-300 input placeholder-current focus:outline-none rounded-xl'
 								placeholder='search'
 								value={text}
-								onChange={handleChange}
 							/>
 							<button
 								type='submit'
@@ -35,11 +50,14 @@ const UserSearch = () => {
 					</div>
 				</form>
 			</div>
-			<div>
-				<button className='btn btn-active rounded-xl hover:bg-gray-300 hover:text-neutral border-none'>
-					Clear
-				</button>
-			</div>
+
+			{users.length > 0 && (
+				<div>
+					<button className='btn btn-active rounded-xl hover:bg-gray-300 hover:text-neutral border-none'>
+						Clear
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
