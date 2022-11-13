d33a00ff702b783c7de28abc1f911ad1f2244fba
Oded <odedatias8115@gmail.com>
2022-11-13 08:46:07 +0200

    logging About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,13 @@
+//	Main component
 const About = () => {
+	//	Main local variables
+	const myRepoUrl = 'https://github.com/odedAtias/Github-Finder';
+
 	return (
 		<div className='hero bg-base-200 rounded-xl'>
 			<div className='hero-content text-center p-10'>
 				<div className='max-w-md'>
+					{/* About Heading */}
 					<h1 className='text-4xl font-bold'>Github Finder</h1>
 					<p className='pt-6'>
 						GitHub Finder is used as an external search engine of GitHub, which
@@ -10,8 +15,11 @@ const About = () => {
 						registered to GitHub.
 					</p>
 					<p className='py-4 font-bold'>Version 1.0.0</p>
+					{/* Link to my project repo */}
 					<a
-						href='https://github.com/odedAtias/Github-Finder'
+						href={myRepoUrl}
+						target='_blank'
+						rel='noreferrer'
 						className='btn btn-sm hover:bg-gray-300 border-none hover:text-neutral rounded-xl sm:btn-sm md:btn-md'>
 						Learn More
 					</a>
