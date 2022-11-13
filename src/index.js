15beae99bfccd1e0d5429f84b7d66fdbb7766fa3
Oded <odedatias8115@gmail.com>
2022-11-13 08:37:57 +0200

    index.js logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+//	React tools imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//	Stylesheet imports
 import './index.css';
+
+//	Components imports
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
